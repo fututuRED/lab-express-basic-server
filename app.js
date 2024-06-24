0694f7033eae7b41b5a8a8201140f163a9f58fcf
Reda Dahmane <r.dahmanee@gmail.com>
2024-06-24 14:45:09 +0200

    not found typo

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.listen(5005, () => {
 });
 
 // app.get("*", (req, res) => {
-//   res.sendFile(__dirname, "/views/nor-found.html");
+//   res.sendFile(__dirname, "/views/not-found.html");
 app.use((req, res, next) => {
-  res.status(404).sendFile(__dirname + "/views/nor-found.html");
+  res.status(404).sendFile(__dirname + "/views/not-found.html");
 });
